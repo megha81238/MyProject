7efe560c5bd870cf643cb4d7c77727603a8772a5
megha81238 <55832749+megha81238@users.noreply.github.com>
2019-09-26 14:42:38 +0530

    create datastructure

---
+++ datastructure.py
@@ -0,0 +1,11 @@
+print("This projects contains files having basic operstions performed on data structure")
+
+print("Basic operations TRAVERSE, INSERTION, DELETION into ARRAY")
+
+print("Basic operations TRAVERSE, INSERTION, DELETION into LINKED LIST")
+
+print("Basic operations TRAVERSE, INSERTION, DELETION into STACK")
+
+print("Basic operations TRAVERSE, INSERTION, DELETION into QUEUE")
+
+print("Basic operations TRAVERSE, INSERTION, DELETION into TREE")
