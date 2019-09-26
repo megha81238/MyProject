dac3182e3ff23c2d734e9633189b2ead950be2f8
megha81238 <55832749+megha81238@users.noreply.github.com>
2019-09-26 14:49:17 +0530

    deletion from array added

--- array.py
+++ array.py
@@ -17,3 +17,22 @@ val=int(input("enter value for insertion"))
 arr.insert(pos,val)
     
 print(arr)
+
+
+#delete element from array
+from array import *
+arr=array('i',[2,4,6,8,10])
+size=5
+x=int(input("enter element for deletion"))
+for i in range(5):
+    if arr[i]==x:
+        index=i
+    else:
+        i+=1
+for i in range(index,size-1,1):
+    arr[i]=arr[i+1]
+size-=1
+
+for i in range(size):
+    print(arr[i])
+print(arr)
